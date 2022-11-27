3c2240ee0693571e8bc320b1e935a9bbb8d09d84
deepika-salve <118427103+deepika-salve@users.noreply.github.com>
2022-11-28 01:15:07 +0530

    Add files via upload

---
+++ OOPS/Q3/Q3TestMain.java
@@ -0,0 +1,10 @@
+package Q3;
+
+public class Q3TestMain {
+	public static void main(String[] args) {
+
+		CustomerAccountStatement customerAccountStatement = new CustomerAccountStatement(101, 44560, "sameer",
+				"1/1/21", "1/1/22", "branch_Obj", 10, 5000.01, 500.01, "23/08/2021");
+		System.out.println(customerAccountStatement);
+	}
+}
